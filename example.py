2fdde9d98c019af0ccfce9ce46833b45abde72a6
Tanjina <tanjinaproma@gmail.com>
2023-12-10 20:26:09 +0600

    example.py modified

--- example.py
+++ example.py
@@ -2,3 +2,4 @@ import pandas as pd
 import numpy as np
 from sklearn.metrics import mean_squared_error, mean_absolute_error, r2_score
 from sklearn.model_selection import train_test_split
+from sklearn.linear_model import ElasticNet
