064ff5d36fcd5e2103365306ee123de43f0b18c1
Tanjina <tanjinaproma@gmail.com>
2023-12-16 21:23:37 +0600

    mlflow log_metric added

--- example.py
+++ example.py
@@ -66,6 +66,8 @@ if __name__=="__main__":
 
         mlflow.log_param("alpha", alpha)
         mlflow.log_param("l1_ratio", l1_ratio)
+        mlflow.log_metric("rmse", rmse)
+        mlflow.log_metric("mae", mae)
 
 
 
