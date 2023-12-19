7cce4727403986d285bbb4882e382b4f0b82afc7
Tanjina <tanjinaproma@gmail.com>
2023-12-19 20:03:18 +0600

    logger modified

--- src/cnnClassifier/__init__.py
+++ src/cnnClassifier/__init__.py
@@ -3,3 +3,7 @@ import sys
 import logging
 
 logging_str = "[%(asctime)s: %(levelname)s: %(module)s: %(message)s]"
+
+log_dir = "logs"
+log_filepath = os.path.join(log_dir,"running_logs.log")
+os.makedirs(log_dir, exist_ok=True)
