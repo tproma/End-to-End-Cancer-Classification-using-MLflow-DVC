964dcc2abda8bf1078b17327b6e6b1ad0821928b
Tanjina <tanjinaproma@gmail.com>
2023-12-19 20:54:24 +0600

    encode image method  added

--- src/cnnClassifier/utils/common.py
+++ src/cnnClassifier/utils/common.py
@@ -135,3 +135,9 @@ def decodeImage(imgstring, fileName):
     with open(fileName, 'wb') as f:
         f.write(imgdata)
         f.close()
+
+
+
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
