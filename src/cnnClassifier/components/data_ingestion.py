2cc967efc9b1ada3406e01a238724617a0b7a270
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-26 22:32:11 +0600

    Create data_ingestion.py

---
+++ src/cnnClassifier/components/data_ingestion.py
@@ -0,0 +1,6 @@
+import os
+import zipfile
+import gdown
+from cnnClassifier import logger
+from cnnClassifier.utils.common import get_size
+from cnnClassifier.entity.config_entity import DataIngestionConfig
