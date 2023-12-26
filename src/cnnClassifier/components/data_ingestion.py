a87f9a812413ba5e35d9bfe9dd9ffc4a4aedcc23
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-26 22:32:56 +0600

    Update data_ingestion.py

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -4,3 +4,10 @@ import gdown
 from cnnClassifier import logger
 from cnnClassifier.utils.common import get_size
 from cnnClassifier.entity.config_entity import DataIngestionConfig
+
+
+class DataIngestion:
+    def __init__(self, config: DataIngestionConfig):
+        self.config = config
+
+
