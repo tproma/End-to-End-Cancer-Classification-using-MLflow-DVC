013ab70720cee037f7f505c403b80fc807383c1c
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-26 22:17:46 +0600

    Update config_entity.py

--- src/cnnClassifier/entity/config_entity.py
+++ src/cnnClassifier/entity/config_entity.py
@@ -1,2 +1,9 @@
 from dataclasss import dataclass
 from pathliib import Path
+
+@dataclass(frozen=True)
+class DataIngestionConfig:
+  root_dir: Path
+  source_url :str
+  local_data_file: Path
+  unzip_dir: Path
