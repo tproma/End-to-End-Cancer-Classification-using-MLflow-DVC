3ea3f2c421221a210ccd3842ed4e04f5d926e46d
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-27 12:30:30 +0600

    extract zip file added in data_ingestion.py

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -31,3 +31,13 @@ class DataIngestion:
         except Exception as e:
             raise e
 
+     def extract_zip_file(self):
+        """
+        zip_file_path: str
+        Extracts the zip file into the data directory
+        Function returns None
+        """
+        unzip_path = self.config.unzip_dir
+        os.makedirs(unzip_path, exist_ok=True)
+        with zipfile.ZipFile(self.config.local_data_file, 'r') as zip_ref:
+            zip_ref.extractall(unzip_path)
