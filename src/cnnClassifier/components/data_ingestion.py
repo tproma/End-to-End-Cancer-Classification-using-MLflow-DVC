ae8be4d0512348f5a47a73e7ed0bfc9e6fecefb6
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-27 12:29:40 +0600

    Update data_ingestion.py

--- src/cnnClassifier/components/data_ingestion.py
+++ src/cnnClassifier/components/data_ingestion.py
@@ -11,3 +11,23 @@ class DataIngestion:
         self.config = config
 
 
+     def download_file(self)-> str:
+        '''
+        Fetch data from the url
+        '''
+
+        try: 
+            dataset_url = self.config.source_URL
+            zip_download_dir = self.config.local_data_file
+            os.makedirs("artifacts/data_ingestion", exist_ok=True)
+            logger.info(f"Downloading data from {dataset_url} into file {zip_download_dir}")
+
+            file_id = dataset_url.split("/")[-2]
+            prefix = 'https://drive.google.com/uc?/export=download&id='
+            gdown.download(prefix+file_id,zip_download_dir)
+
+            logger.info(f"Downloaded data from {dataset_url} into file {zip_download_dir}")
+
+        except Exception as e:
+            raise e
+
