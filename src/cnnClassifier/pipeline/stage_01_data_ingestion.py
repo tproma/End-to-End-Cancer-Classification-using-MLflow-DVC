eebf00e4fb67d92be7387183a0fdf24f9dc00206
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-27 12:34:27 +0600

    Update stage_01_data_ingestion.py

--- src/cnnClassifier/pipeline/stage_01_data_ingestion.py
+++ src/cnnClassifier/pipeline/stage_01_data_ingestion.py
@@ -1,3 +1,11 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.data_ingestion import DataIngestion
 from cnnClassifier import logger
+
+
+
+STAGE_NAME = "Data Ingestion stage"
+
+class DataIngestionTrainingPipeline:
+    def __init__(self):
+        pass
