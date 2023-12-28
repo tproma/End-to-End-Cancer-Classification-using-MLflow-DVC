3d90d041fe2e8bd991d99e1f9341be42f004a5a9
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-28 16:20:02 +0600

    Update prepare_base_model.py

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -12,7 +12,7 @@ class PrepareBaseModel:
         self.config = config
 
 
-      def get_base_model(self):
+    def get_base_model(self):
         self.model = tf.keras.applications.vgg16.VGG16(
             input_shape=self.config.params_image_size,
             weights=self.config.params_weights,
@@ -20,4 +20,35 @@ class PrepareBaseModel:
         )
 
         self.save_model(path=self.config.base_model_path, model=self.model)
+
+
+
+    @staticmethod
+    def _prepare_full_model(model, classes, freeze_all, freeze_till, learning_rate):
+        if freeze_all:
+            for layer in model.layers:
+                model.trainable = False
+        elif (freeze_till is not None) and (freeze_till > 0):
+            for layer in model.layers[:-freeze_till]:
+                model.trainable = False
+
+        flatten_in = tf.keras.layers.Flatten()(model.output)
+        prediction = tf.keras.layers.Dense(
+            units=classes,
+            activation="softmax"
+        )(flatten_in)
+
+        full_model = tf.keras.models.Model(
+            inputs=model.input,
+            outputs=prediction
+        )
+
+        full_model.compile(
+            optimizer=tf.keras.optimizers.SGD(learning_rate=learning_rate),
+            loss=tf.keras.losses.CategoricalCrossentropy(),
+            metrics=["accuracy"]
+        )
+
+        full_model.summary()
+        return full_model
 
