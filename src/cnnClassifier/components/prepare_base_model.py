eb28a7cc4960d78d7234de8cc1fadb66a48b4193
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-28 16:18:37 +0600

    Update prepare_base_model.py

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -10,3 +10,14 @@ from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
 class PrepareBaseModel:
     def __init__(self, config: PrepareBaseModelConfig):
         self.config = config
+
+
+      def get_base_model(self):
+        self.model = tf.keras.applications.vgg16.VGG16(
+            input_shape=self.config.params_image_size,
+            weights=self.config.params_weights,
+            include_top=self.config.params_include_top
+        )
+
+        self.save_model(path=self.config.base_model_path, model=self.model)
+
