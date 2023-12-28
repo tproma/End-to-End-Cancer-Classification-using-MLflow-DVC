3ebce357f9ca5def39b90c2e09f1a317c2abb7f0
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-28 16:18:04 +0600

    Update prepare_base_model.py

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -1,4 +1,3 @@
-
 import os
 import urllib.request as request
 from zipfile import ZipFile
@@ -6,3 +5,8 @@ import tensorflow as tf
 from pathlib import Path
 from cnnClassifier.entity.config_entity import PrepareBaseModelConfig
                                                 
+
+
+class PrepareBaseModel:
+    def __init__(self, config: PrepareBaseModelConfig):
+        self.config = config
