9ee00e197b9e2bda60487435aa770ef027ebd224
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-31 20:32:16 +0600

    Update model_trainer.py

--- src/cnnClassifier/components/model_trainer.py
+++ src/cnnClassifier/components/model_trainer.py
@@ -10,3 +10,8 @@ from pathlib import Path
 class Training:
     def __init__(self, config: TrainingConfig):
         self.config = config
+
+      def get_base_model(self):
+        self.model = tf.keras.models.load_model(
+            self.config.updated_base_model_path
+        )
