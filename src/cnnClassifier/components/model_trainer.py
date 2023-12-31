6e0b3a450a7dbb818c2177bfeb41df6efe9ffca1
Tanjina <tanjinaproma@gmail.com>
2023-12-31 20:38:25 +0600

    constants added

--- src/cnnClassifier/components/model_trainer.py
+++ src/cnnClassifier/components/model_trainer.py
@@ -11,7 +11,7 @@ class Training:
     def __init__(self, config: TrainingConfig):
         self.config = config
 
-      def get_base_model(self):
+    def get_base_model(self):
         self.model = tf.keras.models.load_model(
             self.config.updated_base_model_path
         )
