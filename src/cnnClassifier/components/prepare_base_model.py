51060ad183f98571b15355f70b97067ece3e8ec1
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-31 18:38:04 +0600

    Update base model

--- src/cnnClassifier/components/prepare_base_model.py
+++ src/cnnClassifier/components/prepare_base_model.py
@@ -52,3 +52,18 @@ class PrepareBaseModel:
         full_model.summary()
         return full_model
 
+
+      def update_base_model(self):
+        self.full_model = self._prepare_full_model(
+            model=self.model,
+            classes=self.config.params_classes,
+            freeze_all=True,
+            freeze_till=None,
+            learning_rate=self.config.params_learning_rate
+        )
+
+        self.save_model(path=self.config.updated_base_model_path, model=self.full_model)
+    
+
+
+    
