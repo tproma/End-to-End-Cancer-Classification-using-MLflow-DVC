43d082b0a85c79471f9ea3a82b6af21f7a6e4c32
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-31 20:31:54 +0600

    training class added

--- src/cnnClassifier/components/model_trainer.py
+++ src/cnnClassifier/components/model_trainer.py
@@ -5,3 +5,8 @@ import tensorflow as tf
 import time
 from cnnClassifier.entity.config_entity import TrainingConfig
 from pathlib import Path
+
+
+class Training:
+    def __init__(self, config: TrainingConfig):
+        self.config = config
