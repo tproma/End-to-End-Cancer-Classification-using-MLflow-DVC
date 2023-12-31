09258409002b86154f986e0d0baba137105dd8af
Tanjina Proma <tanjinaproma@gmail.com>
2023-12-31 18:32:56 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,3 @@
+from cnnClassifier import logger
+from cnnClassifier.pipeline.stage_01_data_ingestion import DataIngestionTrainingPipeline
+from cnnClassifier.pipeline.stage_02_prepare_base_model import PrepareBaseModelTrainingPipeline
