1e5b67bc7324d5a32c3a023d38adf47e15b9e409
Tanjina <tanjinaproma@gmail.com>
2024-01-14 18:36:15 +0600

    trainer pipeline modified

--- src/cnnClassifier/pipeline/stage_03_model_trainer.py
+++ src/cnnClassifier/pipeline/stage_03_model_trainer.py
@@ -1,3 +1,10 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.model_trainer import Training
 from cnnClassifier import logger
+
+
+STAGE_NAME = "Training"
+
+class ModelTrainingPipeline:
+    def __init__(self):
+        pass
