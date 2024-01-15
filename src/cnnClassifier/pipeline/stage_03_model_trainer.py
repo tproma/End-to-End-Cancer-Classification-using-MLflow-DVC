f827688c61b32382313aea47ef561271495d4a73
Tanjina <tanjinaproma@gmail.com>
2024-01-15 20:32:35 +0600

    main method added

--- src/cnnClassifier/pipeline/stage_03_model_trainer.py
+++ src/cnnClassifier/pipeline/stage_03_model_trainer.py
@@ -8,3 +8,11 @@ STAGE_NAME = "Training"
 class ModelTrainingPipeline:
     def __init__(self):
         pass
+
+    def main(self):
+        config = ConfigurationManager()
+        training_config = config.get_training_config()
+        training = Training(config=training_config)
+        training.get_base_model()
+        training.train_valid_generator()
+        training.train()
