7eba6478256816e152c4bdcd3c50b662d040ed9e
Tanjina Proma <tanjinaproma@gmail.com>
2024-01-17 23:59:42 +0600

    Update and rename model_evaluation to model_evaluation.py

--- src/cnnClassifier/components/model_evaluation.py
+++ src/cnnClassifier/components/model_evaluation.py
@@ -5,3 +5,7 @@ import mlflow.keras
 from urllib.parse import urlparse
 from cnnClassifier.entity.config_entity import EvaluationConfig
 from cnnClassifier.utils.common import read_yaml, create_directories,save_json
+
+class Evaluation:
+    def __init__(self, config: EvaluationConfig):
+        self.config = config
