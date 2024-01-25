8b68b7b1e655fea29aae9ce46a047f97748ef802
Tanjina <tanjinaproma@gmail.com>
2024-01-25 20:48:15 +0600

    evaluation code updated

--- src/cnnClassifier/components/model_evaluation.py
+++ src/cnnClassifier/components/model_evaluation.py
@@ -10,6 +10,30 @@ class Evaluation:
     def __init__(self, config: EvaluationConfig):
         self.config = config
 
+    def _valid_generator(self):
+
+        datagenerator_kwargs = dict(
+            rescale = 1./255,
+            validation_split=0.30
+        )
+
+        dataflow_kwargs = dict(
+            target_size=self.config.params_image_size[:-1],
+            batch_size=self.config.params_batch_size,
+            interpolation="bilinear"
+        )
+
+        valid_datagenerator = tf.keras.preprocessing.image.ImageDataGenerator(
+            **datagenerator_kwargs
+        )
+
+        self.valid_generator = valid_datagenerator.flow_from_directory(
+            directory=self.config.training_data,
+            subset="validation",
+            shuffle=False,
+            **dataflow_kwargs
+        )
+
 
     @staticmethod
     def load_model(path: Path) -> tf.keras.Model:
