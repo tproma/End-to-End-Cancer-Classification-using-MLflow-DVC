1f62246a7fc8be167e6e2163fd4037ac752c1d6f
Tanjina <tanjinaproma@gmail.com>
2024-03-02 18:37:18 +0600

    evaluation pipeline completed

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -1,3 +1,15 @@
 from cnnClassifier.config.configuration import ConfigurationManager
 from cnnClassifier.components.model_evaluation_mlflow import Evaluation
 from cnnClassifier import logger
+
+
+class EvaluationPipeline:
+    def __init__(self):
+        pass
+
+    def main(self):
+        config = ConfigurationManager()
+        eval_config = config.get_evaluation_config()
+        evaluation = Evaluation(eval_config)
+        evaluation.evaluation()
+        evaluation.log_into_mlflow()
