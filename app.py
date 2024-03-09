4a27e0383a36316f85e0e1f3d7d62f8ff7394bd3
Tanjina <tanjinaproma@gmail.com>
2024-03-09 21:48:29 +0600

    app.py modified

--- app.py
+++ app.py
@@ -4,3 +4,15 @@ from flask_cors import CORS, cross_origin
 from cnnClassifier.utils.common import decodeImage
 from cnnClassifier.pipeline.prediction import PredictionPipeline
 
+
+os.putenv('LANG', 'en_US.UTF-8')
+os.putenv('LC_ALL', 'en_US.UTF-8')
+
+app = Flask(__name__)
+CORS(app)
+
+
+class ClientApp:
+    def __init__(self):
+        self.filename = "inputImage.jpg"
+        self.classifier = PredictionPipeline(self.filename)
