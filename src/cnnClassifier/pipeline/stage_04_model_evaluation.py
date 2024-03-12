4722a7b06b1a5ffa9dd8ff958c1d8d83f99bd1d5
Tanjina <tanjinaproma@gmail.com>
2024-03-12 16:53:52 +0600

    DVC pipeline calls added

--- src/cnnClassifier/pipeline/stage_04_model_evaluation.py
+++ src/cnnClassifier/pipeline/stage_04_model_evaluation.py
@@ -3,6 +3,8 @@ from cnnClassifier.components.model_evaluation_mlflow import Evaluation
 from cnnClassifier import logger
 
 
+STAGE_NAME = "Evaluation stage"
+
 class EvaluationPipeline:
     def __init__(self):
         pass
@@ -14,3 +16,18 @@ class EvaluationPipeline:
         evaluation.evaluation()
         evaluation.save_score()
         evaluation.log_into_mlflow()
+
+
+
+if __name__ == '__main__':
+    try:
+        logger.info(f"*******************")
+        logger.info(f">>>>>> stage {STAGE_NAME} started <<<<<<")
+        obj = EvaluationPipeline()
+        obj.main()
+        logger.info(f">>>>>> stage {STAGE_NAME} completed <<<<<<\n\nx==========x")
+    except Exception as e:
+        logger.exception(e)
+        raise e
+            
+
