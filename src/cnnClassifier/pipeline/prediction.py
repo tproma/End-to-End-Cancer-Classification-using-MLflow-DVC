6d68863c3c6d8802229487da4cdc4ca8b0cd42a1
Tanjina <tanjinaproma@gmail.com>
2024-03-16 14:10:20 +0600

    prediction pipeline modifiied

--- src/cnnClassifier/pipeline/prediction.py
+++ src/cnnClassifier/pipeline/prediction.py
@@ -14,7 +14,7 @@ class PredictionPipeline:
         ## load model
         
         # model = load_model(os.path.join("artifacts","training", "model.h5"))
-        model = load_model(os.path.join("model", "model.h5"))
+        model = load_model(os.path.join("model", "ct_vgg_best_model.hdf5"))
 
         imagename = self.filename
         test_image = image.load_img(imagename, target_size = (224,224))
